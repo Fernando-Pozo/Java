ac503ece6316d42cb091ff4ac0536afab0711969
Fernando Marques <fernandopozomqs@gmail.com>
2022-12-27 01:11:20 -0300

    solução ruim

--- Java POO/Anotações/exceçõesPersonalizadas/Project/src/application/Program.java
+++ Java POO/Anotações/exceçõesPersonalizadas/Project/src/application/Program.java
@@ -27,6 +27,27 @@ public class Program {
 		}else {
 			Reservation reservation = new Reservation(number, checkin, checkOut);
 			System.out.println("Reservation " + reservation);
+			
+			System.out.println();
+			System.out.println("Enter data to update the reservation: ");
+			System.out.print("Check-in date (dd/MM/yyyy): ");
+			checkinS =  sc.next();
+			checkin = LocalDate.parse(checkinS, fmt);
+			System.out.print("Check-out date (dd/MM/yyyy): ");
+			checkOutS =  sc.next();
+			checkOut = LocalDate.parse(checkOutS, fmt);
+			
+			LocalDate now = LocalDate.now();
+			if (checkin.isBefore(now) || checkOut.isBefore(now) ) {
+				System.out.println("Error in reservation: Reservation dates for update must be future dates");
+			}else if(checkOut.isAfter(now)) {
+				System.out.println("Error in reservation: Check-out date must be after check-in date");
+			}else {
+				reservation.updateDates(checkin, checkOut);
+				System.out.println("Reservation " + reservation);
+			}
 		}
+		
+		sc.close();
 	}
 }
