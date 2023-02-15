47a48d54670803fe1681ed48de5bf27a952f6852
Fernando-Pozo <fernandopozomqs@gmail.com>
2023-02-15 13:32:44 -0300

    set

---
+++ Java POO/Anotações/Set/Program/src/application/Program.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Program {
+	
+	public static void main(String[] args) {
+	
+		Set<String> set =  new TreeSet<>();
+		
+		set.add("TV");
+		set.add("Tablet");
+		set.add("Notebook");
+	
+		
+		System.out.println(set.contains("Notebook"));
+		
+		for(String p : set) {
+			System.out.println(p);
+		}
+	}
+	
+}
