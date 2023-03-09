251d2181d587ffbc64e26aef33f98da1723b5973
Fernando-Pozo <fernandopozomqs@gmail.com>
2023-03-09 13:53:42 -0300

    oderitem

--- projeto/course/src/main/java/com/educandoweb/course/entities/Order.java
+++ projeto/course/src/main/java/com/educandoweb/course/entities/Order.java
@@ -2,17 +2,22 @@ package com.educandoweb.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -32,7 +37,13 @@ public class Order implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
-
+	
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
+	
+	//@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	//private Payment payment;
+	
 	public Order() {
 	}
 
@@ -76,6 +87,10 @@ public class Order implements Serializable {
 		if (orderStatus != null) {
 			this.orderStatus = orderStatus.getCode();
 		}
+	}
+	
+	public Set<OrderItem> getItems() {
+		return items;
 	}
 	
 	@Override
